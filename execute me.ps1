d4c15b86e4bbae7178ee6056a49bc1937540a306
zolmine <amine.es.sobhi7@gmail.com>
2021-08-09 01:26:34 +0100

    Initial commit

---
+++ execute me.ps1
@@ -0,0 +1,4 @@
+cd C:\Users\Zolmine\AppData\Roaming\JetBrains\PhpStorm2021.1\
+rmdir "eval" /s /q
+del "options\other.xml"
+reg delete "HKEY_CURRENT_USER\SOFTWARE\JavaSoft\Prefs\jetbrains\phpstorm\" /f
